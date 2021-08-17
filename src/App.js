909269abf8569392d67c951106bd8b95875f6853
aleksandar <alpe88.dev@gmail.com>
2021-08-16 17:20:37 -0700

    formatted

--- src/App.js
+++ src/App.js
@@ -1,81 +1,81 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   NavLink,
-  useRouteMatch
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import getData from './api/getData'
+import getData from "./api/getData";
 
-import Home from '../src/pages/Home';
-import Projects from '../src/pages/Projects';
-import Project from '../src/pages/Project';
-import About from '../src/pages/About';
+import Home from "../src/pages/Home";
+import Projects from "../src/pages/Projects";
+import Project from "../src/pages/Project";
+import About from "../src/pages/About";
 
-import './App.css';
-import '../src/css/animations.css';
-
+import "./App.css";
+import "../src/css/animations.css";
 
 function App() {
-  const [site, setSiteData] = useState({ state: { loaded: false, errors: false }, data: { projects: null, about: null } });
+  const [site, setSiteData] = useState({
+    state: { loaded: false, errors: false },
+    data: { projects: null, about: null },
+  });
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-
-    if(!site.state.loaded) {
-      fetchData().then(
-        r => {
-          setSiteData({ state: { loaded: true }, data: { projects: r[0], about: r[1] } });
-          setLoading(false);
-        }
-      ).catch(e => { 
-        console.log(e);
-        setSiteData({ state: { loaded: true, errors: true } });
-      });
-    }
-
-  }, [site]);
 
   const fetchData = async () => {
     return await getData();
   };
 
+  useEffect(() => {
+    if (!site.state.loaded) {
+      fetchData()
+        .then((r) => {
+          setSiteData({
+            state: { loaded: true },
+            data: { projects: r[0], about: r[1] },
+          });
+          setLoading(false);
+        })
+        .catch((e) => {
+          console.log(e);
+          setSiteData({ state: { loaded: true, errors: true } });
+        });
+    }
+  }, [site]);
+
   console.log(site.data.about);
   console.log(site.data.projects);
   return (
-      <div>
-          { site.state.errors && <p>There were errors!</p>}
-          { loading && <p>Wait I'm Loading for you</p> }
-          { site.state.loaded &&       
-            <Router>
+    <div>
+      {site.state.errors && <p>There were errors!</p>}
+      {loading && <p>Wait I'm Loading for you</p>}
+      {site.state.loaded && (
+        <Router>
+          <Fragment>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/projects">All Projects</NavLink>
+          </Fragment>
 
-              <Fragment>
-                  <NavLink to="/about">About</NavLink>
-                  <NavLink to="/projects">All Projects</NavLink>
-              </Fragment>
-
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route path="/about">
-                  <About data={site.data.about} />
-                </Route>                
-                <Route path="/projects/:slug">
-                  <Project data={site.data.projects} />
-                </Route>
-                <Route path="/projects">
-                  <Projects data={site.data.projects} />
-                </Route>
-                <Route render={() => <h1>Page not found</h1>} />
-              </Switch>
-          </Router>
-          }
-      </div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About data={site.data.about} />
+            </Route>
+            <Route path="/projects/:slug">
+              <Project data={site.data.projects} />
+            </Route>
+            <Route path="/projects">
+              <Projects data={site.data.projects} />
+            </Route>
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </Router>
+      )}
+    </div>
   );
-
 }
 
-export default App
+export default App;

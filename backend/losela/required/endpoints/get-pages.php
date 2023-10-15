16341f80c008e29de0b3bf9e087c9efc8e3114b9
aleksandar <alpe88.dev@gmail.com>
2023-10-15 15:41:06 -0700

    added route for specific pages

--- backend/losela/required/endpoints/get-pages.php
+++ backend/losela/required/endpoints/get-pages.php
@@ -1,9 +1,16 @@
 <?php 
 
-function custom_page_endpoint() {
+function custom_page_endpoints() {
+    // Route to get all pages
     register_rest_route('portfolio-api/v1', '/pages', array(
         'methods' => 'GET',
         'callback' => 'get_all_pages_data',
+    ));
+
+    // Route to get a single page by slug
+    register_rest_route('portfolio-api/v1', '/page/(?P<slug>[\w-]+)', array(
+        'methods' => 'GET',
+        'callback' => 'get_single_page_data',
     ));
 }
 
@@ -25,5 +32,32 @@ function get_all_pages_data($request) {
     return $result;
 }
 
+function get_single_page_data($request) {
+    // Get the slug from the request
+    $slug = $request->get_param('slug');
 
-add_action('rest_api_init', 'custom_page_endpoint');
+    // Query the page by slug
+    $args = array(
+        'name' => $slug,
+        'post_type' => 'page',
+        'posts_per_page' => 1, // Retrieve a single page
+    );
+
+    $page = get_posts($args);
+
+    if (empty($page)) {
+        return new WP_Error('page_not_found', 'Page not found', array('status' => 404));
+    }
+
+    // Wrap the page data in an object
+    $result = array(
+        'page' => $page[0], // Use the first (and only) item in the array
+    );
+
+    // Return the result object
+    return $result;
+}
+
+
+
+add_action('rest_api_init', 'custom_page_endpoints');

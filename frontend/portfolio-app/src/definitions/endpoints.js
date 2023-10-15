5b50d7045925bd0fef89a19f00b75c37dee30666
aleksandar <alpe88.dev@gmail.com>
2023-10-15 15:42:42 -0700

    updated endpoints fetched

--- frontend/portfolio-app/src/definitions/endpoints.js
+++ frontend/portfolio-app/src/definitions/endpoints.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://cdn.aleksandar.online/wp-json/portfolio-api/v1";
 export const ENDPOINTS = {
-  PAGES: `${BASE_URL}/pages`,
-  WORK_EXPERIENCE:  `${BASE_URL}/post-type/work-experience`,
-  RESOURCES:  `${BASE_URL}/post-type/resources`,
-  SITE_INFO:  `${BASE_URL}/site-settings`,
+  ABOUT_PAGE: `${BASE_URL}/page/about`,
+  WORK_EXPERIENCE: `${BASE_URL}/post-type/work-experience`,
+  RESOURCES: `${BASE_URL}/post-type/resources`,
+  SITE_INFO: `${BASE_URL}/site-settings`,
 };

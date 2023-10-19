382e66228aeecea0f4fb614fb8eaa58d5a71e731
aleksandar <alpe88.dev@gmail.com>
2023-10-19 16:46:30 -0700

    added social media definitions

---
+++ frontend/portfolio-app/src/definitions/social-media.js
@@ -0,0 +1,11 @@
+export const PLATFORM = {
+  EMAIL: "email",
+  GITHUB: "github",
+  LINKEDIN: "linkedin",
+};
+
+export const TITLES = {
+  [`${PLATFORM.EMAIL}`]: "Reach out by email.",
+  [`${PLATFORM.GITHUB}`]: "Checkout my Github!",
+  [`${PLATFORM.LINKEDIN}`]: "Connect with me on LinkedIn",
+};

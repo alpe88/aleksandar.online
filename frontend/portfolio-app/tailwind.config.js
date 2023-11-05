9578ea8ce11ffab4518109b811ab434817741a06
aleksandar <alpe88.dev@gmail.com>
2023-11-04 19:27:49 -0700

    plugins and extends added

--- frontend/portfolio-app/tailwind.config.js
+++ frontend/portfolio-app/tailwind.config.js
@@ -1,14 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimated from "tailwindcss-animated";
+import plugin from "tailwindcss/plugin";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    variants: {
-      extend: {},
-    },
-    fontFamily: {
-      heading: ["ArchivoBlack", "sans-serif"],
-      body: ["Lato", "sans-serif"],
+    screens: {
+      xs: "375px",
+      ...defaultTheme.screens,
     },
   },
-  plugins: ["@tailwindcss/typography"],
+  plugins: [
+    "@tailwindcss/typography",
+    tailwindcssAnimated,
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-stroke-sm": (value) => {
+            return {
+              "-webkit-text-stroke": `0.1rem ${value}`,
+            };
+          },
+          "text-stroke-base": (value) => {
+            return {
+              "-webkit-text-stroke": `0.2rem ${value}`,
+            };
+          },
+          "text-stroke-lg": (value) => {
+            return {
+              "-webkit-text-stroke": `0.3rem ${value}`,
+            };
+          },
+          "text-stroke-xl": (value) => {
+            return {
+              "-webkit-text-stroke": `0.5rem ${value}`,
+            };
+          },
+        },
+        { values: flattenColorPalette(theme("colors")) }
+      );
+    }),
+  ],
 };
